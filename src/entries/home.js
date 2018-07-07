9cd6076d5669123d5c4ca127aa8a5e6cb9937c5e
Julio Hurtado <juliohurtado.backend@gmail.com>
2018-07-07 00:05:13 -0500

    Add css and style loadders

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+import '../styles/home.css'
+
 const app = document.getElementById('app')
 
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className='Home'>
         <p>Hola Mundo desde Reactjs :D</p>
       </div>
     )
